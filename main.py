1c9df268ebd17971834efcb62e20cf90f3cef235
pro99d <pro999999d@gmail.com>
2026-01-27 18:37:11 +0300

    basic window

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import arcade
+import base_classes as bc
+
+
+class Window(arcade.Window):
+    def __init__(self):
+        super().__init__(800, 600, "game for game jam")
+
+    def on_draw(self):
+        self.clear()
+
+    def on_key_press(self, key, *_):
+        if key == arcade.key.Q:
+            arcade.close_window()
+
+
 def main():
-    print("Hello from gamejam!")
+    win = Window()
+    win.run()
 
 
 if __name__ == "__main__":
