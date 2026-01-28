cba707b6b34ead74b54c7b5558b9df29b4a6b21c
pro99d <pro999999d@gmail.com>
2026-01-28 17:29:01 +0300

    added bloom filter

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 import arcade
 import base_classes as bc
 
+
+
 class Window(arcade.Window):
     def __init__(self):
         super().__init__(800, 600, "game for game jam")
+        self.bloom = arcade.experimental.BloomFilter(self.width, self.height, 20)
+        # self.bloom.fbo.use()
+        # self.bloom.draw(0, self.ctx)
+    def on_resize(self, width: int, height: int):
+        self.bloom = arcade.experimental.BloomFilter(width, height, 20)
 
     def on_draw(self):
         self.clear()
+        self.bloom.fbo.use()
+        with self.bloom.fbo: 
+            arcade.draw_circle_filled(100, 100, 10, (255, 255, 255))
+
+        self.ctx.screen.use()
+        self.bloom.draw(0, self.ctx.screen)
 
     def on_key_press(self, key, *_):
         if key == arcade.key.Q:
